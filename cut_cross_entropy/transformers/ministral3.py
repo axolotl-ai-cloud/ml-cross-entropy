c814521c75f35473f3a17ffcee0db685492d61ae
NanoCode012 <nano@axolotl.ai>
2025-12-02 11:16:04 +0700

    feat: add ministral

---
+++ cut_cross_entropy/transformers/ministral3.py
@@ -0,0 +1,73 @@
+"""Ministral 1/3 CCE patch. Both inherits Llama. Adapted from transformers 2dba972."""
+
+# Copyright (C) 2024 Apple Inc. All Rights Reserved.
+
+# Copyright 2024 Axolotl AI. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from types import MethodType
+
+import transformers
+
+from cut_cross_entropy.transformers.utils import (
+    PatchOptions,
+    TransformersModelT,
+)
+
+
+def patch_ministral(
+    maybe_model: TransformersModelT | str | transformers.PretrainedConfig,
+    patch_options: PatchOptions,
+) -> TransformersModelT | None:
+    # Set the _PATCH_OPTS in the llama patch file
+    from . import llama as llama_patch
+
+    llama_patch._PATCH_OPTS = patch_options
+
+    cce_forward = llama_patch.cce_forward
+
+    from transformers.models.ministral import modeling_ministral
+
+    if isinstance(maybe_model, transformers.PreTrainedModel):
+        assert isinstance(
+            maybe_model, modeling_ministral.MinistralForCausalLM
+        ), f"Expected a MinistralForCausalLM model. Got {type(maybe_model)}."
+        maybe_model.forward = MethodType(cce_forward, maybe_model)
+        return maybe_model
+
+    modeling_ministral.MinistralForCausalLM.forward = cce_forward
+    return None
+
+def patch_ministral3(
+    maybe_model: TransformersModelT | str | transformers.PretrainedConfig,
+    patch_options: PatchOptions,
+) -> TransformersModelT | None:
+    # Set the _PATCH_OPTS in the llama patch file
+    from . import llama as llama_patch
+
+    llama_patch._PATCH_OPTS = patch_options
+
+    cce_forward = llama_patch.cce_forward
+
+    from transformers.models.ministral3 import modeling_ministral3
+
+    if isinstance(maybe_model, transformers.PreTrainedModel):
+        assert isinstance(
+            maybe_model, modeling_ministral3.Ministral3ForCausalLM
+        ), f"Expected a Ministral3ForCausalLM model. Got {type(maybe_model)}."
+        maybe_model.forward = MethodType(cce_forward, maybe_model)
+        return maybe_model
+
+    modeling_ministral3.Ministral3ForCausalLM.forward = cce_forward
+    return None
