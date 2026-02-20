58d6572bef6e6e7fd41d3852441b31b28fd23222
NanoCode012 <nano@axolotl.ai>
2026-02-20 18:21:14 +0700

    feat: add glm_moe_dsa (glm5) patch (#45)

---
+++ cut_cross_entropy/transformers/glm_moe_dsa.py
@@ -0,0 +1,60 @@
+"""GLM MoE DSA (GLM-5) CCE patch. GLM MoE DSA inherits GLM4 MoE which inherits Llama. Adapted from transformers 5.2.0."""
+
+# Copyright (C) 2024 Apple Inc. All Rights Reserved.
+
+# Copyright 2024 Axolotl AI. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from types import MethodType
+
+import transformers
+
+from cut_cross_entropy.transformers.utils import (
+    PatchOptions,
+    TransformersModelT,
+    patch_remote_model_class,
+)
+
+
+def patch_glm_moe_dsa(
+    maybe_model: TransformersModelT | str | transformers.PretrainedConfig,
+    patch_options: PatchOptions,
+    remote_model_id: str | None = None,
+) -> TransformersModelT | None:
+    # Set the _PATCH_OPTS in the llama patch file
+    from . import llama as llama_patch
+
+    llama_patch._PATCH_OPTS = patch_options
+
+    cce_forward = llama_patch.cce_forward
+
+    if remote_model_id is not None:
+        patch_remote_model_class(
+            remote_model_id=remote_model_id,
+            class_name="GlmMoeDsaForCausalLM",
+            patch_fn=cce_forward,
+        )
+        return None
+
+    from transformers.models.glm_moe_dsa import modeling_glm_moe_dsa
+
+    if isinstance(maybe_model, transformers.PreTrainedModel):
+        assert isinstance(maybe_model, modeling_glm_moe_dsa.GlmMoeDsaForCausalLM), (
+            f"Expected a GlmMoeDsaForCausalLM model. Got {type(maybe_model)}."
+        )
+        maybe_model.forward = MethodType(cce_forward, maybe_model)
+        return maybe_model
+
+    modeling_glm_moe_dsa.GlmMoeDsaForCausalLM.forward = cce_forward
+    return None
