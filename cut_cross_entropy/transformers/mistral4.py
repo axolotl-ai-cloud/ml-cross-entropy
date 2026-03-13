4f8da7380fe36d07c02e65939346d965e054e4a4
NanoCode012 <nano@axolotl.ai>
2026-03-14 00:26:56 +0700

    feat: add mistral4

---
+++ cut_cross_entropy/transformers/mistral4.py
@@ -0,0 +1,58 @@
+"""Mistral4 CCE patch. Mistral4 inherits Llama. Adapted from Mistral4 PR"""
+
+# Copyright (C) 2024 Apple Inc. All Rights Reserved.
+
+# Copyright 2024 Axolotl AI. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from types import MethodType
+
+import transformers
+
+from cut_cross_entropy.transformers.utils import (
+    PatchOptions,
+    TransformersModelT,
+    patch_remote_model_class,
+)
+
+
+def patch_mistral4(
+    maybe_model: TransformersModelT | str | transformers.PretrainedConfig,
+    patch_options: PatchOptions,
+    remote_model_id: str | None = None,
+) -> TransformersModelT | None:
+    from . import llama as llama_patch
+
+    llama_patch._PATCH_OPTS = patch_options
+    cce_forward = llama_patch.cce_forward
+
+    if remote_model_id is not None:
+        patch_remote_model_class(
+            remote_model_id=remote_model_id,
+            class_name="Mistral4ForCausalLM",
+            patch_fn=cce_forward,
+        )
+        return None
+
+    from transformers.models.mistral4 import modeling_mistral4
+
+    if isinstance(maybe_model, transformers.PreTrainedModel):
+        assert isinstance(maybe_model, modeling_mistral4.Mistral4ForCausalLM), (
+            f"Expected a Mistral4ForCausalLM model. Got {type(maybe_model)}."
+        )
+        maybe_model.forward = MethodType(cce_forward, maybe_model)
+        return maybe_model
+
+    modeling_mistral4.Mistral4ForCausalLM.forward = cce_forward
+    return None
