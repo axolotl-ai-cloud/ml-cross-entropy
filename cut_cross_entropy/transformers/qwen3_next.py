3428346b540371e48fad964dfadfae6cfbba0b51
NanoCode012 <nano@axolotl.ai>
2026-06-04 16:24:16 +0700

    chore: docstring update

--- cut_cross_entropy/transformers/qwen3_next.py
+++ cut_cross_entropy/transformers/qwen3_next.py
@@ -1,4 +1,4 @@
-"""Qwen3_next CCE patch. It inherits Mixtral. Adapted from transformers Qwen3_next PR."""
+"""Qwen3_next CCE patch. It inherits Mixtral. Adapted from transformers 5.10.1."""
 
 # Copyright (C) 2024 Apple Inc. All Rights Reserved.
 
